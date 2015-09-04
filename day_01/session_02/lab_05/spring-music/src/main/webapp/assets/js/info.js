6d1becd820782a1804a54f8681506642f7843b44
whewatt-pivotal <whewatt@pivotal.io>
2015-09-04 13:26:19 -0400

    Edit

--- day_01/session_02/lab_05/spring-music/src/main/webapp/assets/js/info.js
+++
@@ -1,8 +0,0 @@
-angular.module('info', ['ngResource']).
-    factory('Info', function ($resource) {
-        return $resource('info');
-    });
-
-function InfoController($scope, Info) {
-    $scope.info = Info.get();
-}
