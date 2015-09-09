1854101255b7d7bffff4ef65d0a75752ff514e77
whewatt-pivotal <whewatt@pivotal.io>
2015-09-09 14:16:32 -0400

    Edits

--- day_02/session_05/lab_14/complete/springbox-recommendations/src/main/java/io/springbox/recommendations/repositories/LikesRepository.java
+++
@@ -1,7 +0,0 @@
-package io.springbox.recommendations.repositories;
-
-import io.springbox.recommendations.domain.Likes;
-import org.springframework.data.neo4j.repository.GraphRepository;
-
-public interface LikesRepository extends GraphRepository<Likes> {
-}
--- day_02/session_05/lab_14/complete/springbox-reviews/src/main/java/io/springbox/reviews/repositories/ReviewRepository.java
+++
@@ -1,8 +0,0 @@
-package io.springbox.reviews.repositories;
-
-import io.springbox.reviews.domain.Review;
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-public interface ReviewRepository extends MongoRepository<Review, String> {
-    Iterable<Review> findByMlId(String mlId);
-}
